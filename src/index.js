3b56e7f3029bdef782c394c8da9e8f7609911ec7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-18 15:10:56 +1100

    Updare Board logic to determine next player and if a player wins

--- src/index.js
+++ src/index.js
@@ -24,14 +24,21 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   
   // When the square component is clicked, this is the function that is called
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    // if (squares[i]===null) {
+    
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+    // }
   }
 
   // Each square is rendered as a Square component while passing value and onClick
@@ -42,7 +49,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -91,3 +104,28 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+// Declare a winner
+function calculateWinner(squares) {
+  // Winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    // If 3 connected squares (a, b, c) are equal
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
